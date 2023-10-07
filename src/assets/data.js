37e3ac0609373ad6589b707ca40294248eefba1a
ycheryl <yulcheryl2357@gmail.com>
2023-10-07 10:00:24 +0700

    add asset

---
+++ src/assets/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: 1,
+        title: "Lecture 1: Greeting, Nationality, Direction, Possession",
+        topic: "Express Courtesy, Self-introduction",
+        notes: "", 
+        slide: "https://www.canva.com/design/DAFnKIHO98s/a1PU3ZqlhdKre0wjM3bZNg/view?website#2:1",
+        challenge: "",
+        newVoc: "",
+        current: false
+    }, 
+    {
+        id: 2,
+        title: "Lecture 2: Number, Time, Direction, Transportation",
+        topic: "Checkout the shopping bill, Schedule a day, Describe common things in home", 
+        notes: "", 
+        slide: "https://www.canva.com/design/DAFnosRVOY0/Td4hDy6RGjOoww1SPjRc_w/view?website#2",
+        challenge: "",
+        newVoc: "",
+        current: true
+    }
+]
+
+export default data
