0ef381fa4b5a43ee821cf795430c443d543633c2
ycheryl <yulcheryl2357@gmail.com>
2023-10-07 08:28:07 +0700

    add pages

---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Blogs() {
+    return (
+        <h1>This is my blog</h1>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import ava from "../logo.svg"
+
+export default function Home(props) {
+    return (
+    <div className={`home-container ${props.mode ? "dark" : ""}`} >
+        <div className="home--header">
+            <h1>HELLO WORLD</h1>
+            <div className="home--animation">
+                <div className="animation-object one"></div>
+                <div className="animation-object two"></div>
+                <div className="animation-object three"></div>
+                <div className="animation-object four"></div>
+                <div className="animation-object five"></div>
+            </div>
+            <img src={ava} className="home--image" alt="yul's avatar" />
+        </div>
+        <div className="home--content">
+            <p>Hi, I'm Yul.</p>
+            <p>Recently, ...</p>
+        </div>
+    </div>
+    )
+}
