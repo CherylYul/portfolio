e42c0bcb9560ffa625e4d01a87aefc3147b28466
ycheryl <yulcheryl2357@gmail.com>
2023-11-12 23:15:41 +0700

    add project list page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,18 @@
 import React from "react"
+import projectsList from "../assets/projectsList"
 
 export default function Projects() {
+    const projectsEl = projectsList.map(data => 
+    <div key={data.id} className="project-el">
+        <h3 className="project-title">{data.name}</h3>
+        <p className="project-desc">{data.desc}</p>
+        {data.skills.map(skill => <button className="project-skill">{skill}</button>)}
+    </div>    
+    )
+
     return (
-        <h1>This is my projects</h1>
+        <div className="projects-list-container">
+            {projectsEl}
+        </div>
     )
 }
