6c60dc15a2dab34fa5e907cf37b268c7fbd3ebdf
ycheryl <yulcheryl2357@gmail.com>
2023-11-14 12:55:13 +0700

    add data

---
+++ src/assets/data/blog.js
@@ -0,0 +1,16 @@
+const blogsList = [
+    {
+        id: 1,
+        title: "My starting journey to software engineer",
+        tags: ["life", "career"],
+        date: "Oct 27, 2023"
+    },
+    {
+        id: 2,
+        title: "10 impressive problems related to recursion",
+        tags: ["computer science"],
+        date: "Nov 4, 2023"
+    }
+]
+
+export default blogsList
