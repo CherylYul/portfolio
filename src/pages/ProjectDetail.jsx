9553fe19a08200c20d07a69c4ee833e414fd7e7e
ycheryl <yulcheryl2357@gmail.com>
2023-11-16 00:43:36 +0700

    add params id

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -1,10 +1,16 @@
 import React from "react"
+import { useParams } from "react-router-dom"
+import SpaceX from "../projects/spacex"
+import SUFfolio from "../projects/suffolio"
 
 export default function ProjectDetail(props) {
+    const params = useParams()
+    const content = [<SUFfolio />, <SpaceX />]
+
     return (
         <div className={`project-detail-container ${props.mode ? "dark" : ""}`}>
             <div className="center-container">
-                <h1>Write something...</h1>
+                {content[params.id]}
             </div>
         </div>
     )
