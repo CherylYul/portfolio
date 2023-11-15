6969493d63cae06f6f5ef2e7c68e04f97cf62d43
ycheryl <yulcheryl2357@gmail.com>
2023-11-16 00:42:35 +0700

    add center container

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,31 +20,33 @@ export default function Footer(props) {
 
     return (
     <footer className={props.mode ? "dark" : ""}>
-        <div className="footer--left">
-            <p>I'm always ready to listening to interesting stories and sincere feedback,
-                or having useful conversations as well as meaningful collaboration.</p>
-            <form onSubmit={handleSubmit}>
-                <input type="text" 
-                    placeholder="Your email"
-                    onChange={handleChange}
-                    name="email"
-                    value={formData.email} 
-                    className="cfd9de-border"/>
-                <textarea onChange={handleChange} 
-                    placeholder="Would you like to share somethings?"
-                    name="content"
-                    value={formData.content}
-                    className="cfd9de-border" />
-                <button className="cfd9de-border">Send</button>
-            </form>
-        </div>
-        <div className="footer--right">
-            <h4>Contact me.</h4>
-            <div className="contact-container">
-                <div><i class="bi bi-github"></i><p>Github</p></div>
-                <div><i class="bi bi-linkedin"></i><p>LinkedIn</p></div>
-                <div><i class="bi bi-twitter"></i><p>Twitter</p></div>
-                <div><i class="bi bi-envelope"></i><p>Email</p></div>
+        <div className="center-container">
+            <div className="footer--left">
+                <p>I'm always ready to listening to interesting stories and sincere feedback,
+                    or having useful conversations as well as meaningful collaboration.</p>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" 
+                        placeholder="Your email"
+                        onChange={handleChange}
+                        name="email"
+                        value={formData.email} 
+                        className="cfd9de-border"/>
+                    <textarea onChange={handleChange} 
+                        placeholder="Would you like to share somethings?"
+                        name="content"
+                        value={formData.content}
+                        className="cfd9de-border" />
+                    <button className="cfd9de-border">Send</button>
+                </form>
+            </div>
+            <div className="footer--right">
+                <h4>Contact me.</h4>
+                <div className="contact-container">
+                    <div><i className="bi bi-github"></i><p>Github</p></div>
+                    <div><i className="bi bi-linkedin"></i><p>LinkedIn</p></div>
+                    <div><i className="bi bi-twitter"></i><p>Twitter</p></div>
+                    <div><i className="bi bi-envelope"></i><p>Email</p></div>
+                </div>
             </div>
         </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,25 @@ import { Link } from "react-router-dom"
 export default function Header(props) {
     return (
         <header className={props.mode ? "dark" : ""}>
-            <Link className="site-main" to="/">YulCheryl</Link>
-            <nav>
-                <Link className="site-about" to="./about">About</Link>
-                <Link className="site-projects" to="./projects">Projects</Link>
-                <Link className="site-blogs" to="./blogs">Blogs</Link>
-                <Link className="site-chinese" to="./chinese">Chinese</Link>
-            </nav>
-            <div className="theme">
-                <p className={props.mode ? "" : "bold"}>Light</p>
-                <div className={`toggle-container ${props.mode ? "dark" : ""}`}>
-                    <div className="toggle-btn" onClick={props.handleClick}></div>
+            <div className="center-container">
+                <div className="header--left">
+                    <Link className="site-main" to="/">YulCheryl</Link>
                 </div>
-                <p className={props.mode ? "bold" : ""}>Dark</p>
+                <div className="header--right">
+                    <nav>
+                        <Link className="site-about" to="./about">About</Link>
+                        <Link className="site-projects" to="./projects">Projects</Link>
+                        <Link className="site-blogs" to="./blogs">Blogs</Link>
+                        <Link className="site-chinese" to="./chinese">Chinese</Link>
+                    </nav>
+                    <div className="theme">
+                        <p className={props.mode ? "" : "bold"}>Light</p>
+                        <div className={`toggle-container ${props.mode ? "dark" : ""}`}>
+                            <div className="toggle-btn" onClick={props.handleClick}></div>
+                        </div>
+                        <p className={props.mode ? "bold" : ""}>Dark</p>
+                    </div>
+                </div>
             </div>
         </header>
     )
