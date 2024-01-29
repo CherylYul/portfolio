4805d3b7a62cc18e63436def1c54371e42b6f0e5
ycheryl <yulcheryl2357@gmail.com>
2024-01-29 23:24:32 +0700

    adjust data

--- src/assets/data/blog.js
+++ src/assets/data/blog.js
@@ -4,14 +4,14 @@ const blogsList = [
         title: "10 impressive problems related to recursion",
         tags: ["recursion"],
         date: "Nov 15, 2023",
-        desc: "Everyone in computer science knows the term “recursion”. Recursion is a strange yet magical solutions especially in programming, this method we’d never heard before in high school math until we actually enroll into a course or taking a class of programming."
+        desc: "Everyone in computer science is familiar with the term 'recursion.' It is a peculiar yet magical solution, particularly in programming. This method was unheard of during our high school math days until we enrolled in a programming course."
     },
     {
         id: 1,
         title: "My starting journey to software engineer",
         tags: ["life", "career"],
         date: "Oct 27, 2023", 
-        desc: "I still woke up everyday, took on the bus, went to the company, but somewhere in the deep reaches of my consciousness told me that my soul has belonged to the job that I would be involved in coding and thinking the way to make some lines of code work."
+        desc: "I still woke up every day, took the bus, and went to the company, but somewhere in the deep recesses of my consciousness, I felt that my soul belonged to the job that involved coding and figuring out ways to make lines of code work."
     },
 ]
 
--- src/assets/data/project.js
+++ src/assets/data/project.js
@@ -3,13 +3,13 @@ const projectsList = [
         id: 0,
         name: "SUFfolio",
         skills: ["ReactJS", "Github Pages", "TailwindCSS"],
-        desc: "A stable, useful, and flexble portfolio that provides your audience a window into your world where you can generate credibility, store achievements, and write your own blogs.",
+        desc: "A stable, useful, and flexible portfolio that offers your audience a glimpse into your world. It serves as a platform to build credibility, showcase achievements, and share your insightful blogs.",
     },
     {
         id: 1,
         name: "SpaceX first stage landing prediction",
         skills: ["Python", "Scraping", "EDA", "Dashboard", "Machine Learning"],
-        desc: "An attempt to reduce the cost of launching Falcon 9 into space by predicting the percentage of successfully landing the first stage given the API and web data on SpaceX and wikipedia.",
+        desc: "An attempt to reduce the cost of launching Falcon 9 into space involves predicting the percentage of successfully landing the first stage using the API and web data from SpaceX and Wikipedia.",
     }
 ]
 
